43d306009d38c240c890268a23ef9667ee308ea9
Cody Smith <scody49@gmail.com>
2026-05-20 14:32:18 -0700

    Create home/login page

---
+++ ENVibe/src/pages/Home.jsx
@@ -0,0 +1,24 @@
+
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import './App.css'
+
+function Home() {
+    return (
+        <>
+            <div style={{ 
+                backgroundImage: `../background.jpg`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                height: '100vh' 
+                }}>
+            </div>
+            <nav>
+                <Link to='/'>Home</Link>
+            </nav>
+            <div>
+                <h1>Welcome to ENVibe. Now let's get started.</h1>
+            </div>
+        </>
+    )
+}
