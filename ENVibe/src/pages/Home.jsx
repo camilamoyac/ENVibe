1606af77ae675dc2719257cf82a8eac8fa727c84
Cody Smith <scody49@gmail.com>
2026-05-21 21:56:43 -0700

    Home

--- ENVibe/src/pages/Home.jsx
+++ ENVibe/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import './App.css'
+import '../App.css'
 
 function Home() {
     return (
