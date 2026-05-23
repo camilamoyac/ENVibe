28fbdc029682e8330934d5a75edea20fc74ff0e7
Cody Smith <scody49@gmail.com>
2026-05-22 20:22:37 -0700

    create home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,21 @@
+
+import { Link } from "react-router-dom";
+import "../App.css"
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <section className="hero">
+      <nav className="home-nav">
+        <img src="#" alt="Logo" />
+        <Link className="links">
+          <p className="login">Login</p>
+        </Link>
+      </nav>
+          <div>
+            <h1 className="home-h1">Welcome to ENVibe</h1>
+            <p className="home-p">This website is created to give you the music that you're feeling. Wether you're reading, cooking, exercising and you want music to fit your mood we've got you. <br></br>Now what are you waiting for, start your playlist today.</p>
+            <button className="home-btn">Create Vibe</button>
+          </div>
+    </section>
   );
 }
